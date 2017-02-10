6d690f78dac15544a3188957cf87dce139bd694d
Hasneet Singh <hasneet.singh96@gmail.com>
2017-02-10 22:41:38 +0530

    first commit

---
+++ app/src/main/java/com/example/hasneetsingh/projectabc/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.hasneetsingh.projectabc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
